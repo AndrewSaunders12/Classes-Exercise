011ee000e3f768f7f938829a52b785968f81667a
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-04-03 23:20:39 -0400

    cars n stuff

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "ford";
+            myCar.Model = "GT";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("AMG", "GTR", 2020);
+
+            var carList = new List<Car>() { myCar, mazda, chevy, };
+           
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
         }
     }
 }
